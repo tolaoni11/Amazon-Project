7dc793b0a116f2bcfc2ef1549d0d64b6eb70d3fb
tolaoni11 <tolaoni11@yahoo.com>
2025-06-18 16:46:34 -0500

    deletes products from HTML after clicking 'delete'

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) =>{
   console.log(matchingProduct);
 
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -105,5 +105,9 @@ document.querySelectorAll('.js-delete-link')
      const productId = link.dataset.productId
      removeFromCart(productId);
      console.log(cart)
+
+     const container = document.querySelector(`.js-cart-item-container-${productId}`)
+
+     container.remove();
     })
   })
