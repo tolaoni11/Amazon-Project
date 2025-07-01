acde1dcfa7d341db0b52fef2a269c25e340005f7
tolaoni11 <tolaoni11@yahoo.com>
2025-07-01 15:34:42 -0500

    Sava data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,10 +1,10 @@
 import { cart, removeFromCart, 
   calculateCartQuantity, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js"; 
+import { products, getProduct } from "../../data/products.js"; 
 import { formatCurrency } from "../utils/money.js"; 
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 
 
 hello();
@@ -16,23 +16,12 @@ export function renderOrderSummary() {
   cart.forEach((cartItem) =>{
     const productId = cartItem.productId
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-      if (product.id === productId) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId)
     
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    })
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays,'days')
